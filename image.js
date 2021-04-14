095db38500ba487cad93064b386de1196672727f
takeuma0825 <ayc0825@gmail.com>
2021-04-14 09:53:10 +0900

    本番操作中を示す

---
+++ image.js
@@ -0,0 +1,4 @@
+$('#vue-header').append(`<div id="image"></div>`)
+//$('#vue-header').append(`<img src="images/prod.png"/>`)
+$('.ehypAl').on('click', function() {alert("本番環境")})
+console.log("chrome extends test")
